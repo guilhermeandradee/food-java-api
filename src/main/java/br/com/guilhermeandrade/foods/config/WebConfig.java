4bc568e070fad102fbe04e52d0956420ed0a83a3
Guilherme Andrade <96486731+guilhermeandradee@users.noreply.github.com>
2024-07-10 09:04:05 -0300

    corsConfig

--- src/main/java/br/com/guilhermeandrade/foods/config/WebConfig.java
+++ src/main/java/br/com/guilhermeandrade/foods/config/WebConfig.java
@@ -15,7 +15,7 @@ public class WebConfig implements WebMvcConfigurer {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("*") // Origem do seu frontend
+                        .allowedOriginPatterns("*")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
